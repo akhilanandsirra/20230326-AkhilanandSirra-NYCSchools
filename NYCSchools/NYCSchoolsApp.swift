9e367cf6d2cebd8fa716e8eba60ea6a712a61223
akhilanandsirra <akhilanand9999@gmail.com>
2023-03-23 20:29:30 -0400

    Initial Commit

---
+++ NYCSchools/NYCSchoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYCSchoolsApp.swift
+//  NYCSchools
+//
+//  Created by Akhil Anand Sirra on 23/03/23.
+//
+
+import SwiftUI
+
+@main
+struct NYCSchoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
